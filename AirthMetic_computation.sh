965137f22e2b0e6d699b5a316d0ade69da4da5fb
patelashok <ashok.patel2917@gmail.com>
2021-05-01 23:44:56 +0530

    ADDED: sort in Decending order

--- AirthMetic_computation.sh
+++ AirthMetic_computation.sh
@@ -17,5 +17,24 @@ compute[compute2]=$compute2
 compute[compute3]=$compute3
 compute[compute4]=$compute4
 echo ${compute[@]}
-array=($compute)
-echo ${compute[@]}
+arr=($compute1 $compute2 $compute3 $compute4)
+echo ${arr[@]}
+
+for ((i = 0; i<4; i++))
+do
+      
+    for((j = 0; j<4-i-1; j++))
+    do
+      
+        if [ ${arr[j]} -lt ${arr[$((j+1))]} ]
+        then
+            # swap
+            temp=${arr[j]}
+            arr[$j]=${arr[$((j+1))]}  
+            arr[$((j+1))]=$temp
+        fi
+    done
+done
+  
+echo "Array in sorted Decending order :"
+echo ${arr[*]}
