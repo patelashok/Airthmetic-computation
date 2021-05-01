fbc4d4d095e4391eeb53a4aa1162d1a7a05c0c52
patelashok <ashok.patel2917@gmail.com>
2021-05-01 23:06:44 +0530

    ADDED:store compute value in Dictionary

--- AirthMetic_computation.sh
+++ AirthMetic_computation.sh
@@ -11,3 +11,9 @@ compute3=$((c+a/b))
 echo $compute3
 compute4=$((a%b+c))
 echo $compute4
+declare -A compute
+compute[compute1]=$compute1
+compute[compute2]=$compute2
+compute[compute3]=$compute3
+compute[compute4]=$compute4
+echo ${compute[@]}
